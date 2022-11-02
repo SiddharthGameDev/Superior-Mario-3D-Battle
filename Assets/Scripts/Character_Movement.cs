2ff3f42d13df9d89893fa859821b180437dc0b20
SiddharthGameDev <72736772+SiddharthGameDev@users.noreply.github.com>
2022-11-02 23:46:39 +0530

    Update Character Animation
    The character is Animated and walks, runs, and jumps perfectly.

--- Assets/Scripts/Character_Movement.cs
+++ Assets/Scripts/Character_Movement.cs
@@ -5,50 +5,109 @@ using UnityEngine;
 
 public class Character_Movement : MonoBehaviour
 {
-    
+    [Range(1,10)]public float mouseSensitivity = 5;
+    public float rotationSnapMultiplier = 5f;
     Animator animator;
     private Rigidbody RB;    
     public float Speed = 8.0f;
-    public Camera followCamera;
+    public Transform cameraPivot;
     private Vector3 CameraPos;
-  
+    public bool rotateCameraWithMouse = false;
+    Collider col;
 
+    private Vector3 mousePos;
+    Transform activeCamera;
+
+    [SerializeField]Vector3 globalDirection;
+    Quaternion targetRotation;
+
+    public float GroundCheck_Radius = 0.1f;
+    public Vector3 GroundCheck_RaycastStartOffset;
+    public LayerMask GroundCheck_LayerMask;
+
+    public float JumpVelocity = 19.62f;
+    bool isGrounded = false, isJumping;
 
     private void Awake()
     {
         RB = GetComponent<Rigidbody>();
-        CameraPos = followCamera.transform.position - transform.position;
-        animator = GetComponent<Animator>();
+        CameraPos = cameraPivot.transform.position - transform.position;
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        mousePos = Input.mousePosition;
+        col = GetComponent<Collider>();
     }
-    
-    
+
+    private void Start()
+    {
+        activeCamera = Camera.main.transform;
+    }
+
     void FixedUpdate()
     {
+        Vector3 output = globalDirection * Speed;
+        RB.velocity = new Vector3(output.x, RB.velocity.y, output.z);//=RB.position * Time.deltaTime;
+
+        if (globalDirection != Vector3.zero)
+        {
+            RB.MoveRotation(targetRotation);
+        }
+
+        col.enabled = false;
+        isGrounded = Physics.CheckSphere(this.transform.position + GroundCheck_RaycastStartOffset, GroundCheck_Radius, GroundCheck_LayerMask);
+        col.enabled = true;
+        if (isGrounded == true) isJumping = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        /*Gizmos.DrawLine(this.transform.position + GroundCheck_RaycastStartOffset, 
+            this.transform.position + GroundCheck_RaycastStartOffset + Vector3.down * GroundCheck_Radius);*/
+        Gizmos.DrawWireSphere(this.transform.position + GroundCheck_RaycastStartOffset, GroundCheck_Radius);
+    }
+
+    private void Update()
+    {
+        
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
 
-        if(movement == Vector3.zero)
+        globalDirection = activeCamera.TransformDirection(movement);
+        globalDirection.y = 0;
+        globalDirection.Normalize();
+
+        //global to local space
+        //clipspace
+
+        if (globalDirection != Vector3.zero)
         {
-            return;
+            targetRotation = Quaternion.LookRotation(globalDirection);
+            // Debug.Log(targetRotation.eulerAngles);
+            targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360 * Time.fixedDeltaTime * rotationSnapMultiplier);
         }
 
-        Quaternion targetRotation = Quaternion.LookRotation(movement);
-       // Debug.Log(targetRotation.eulerAngles);
-        targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360 * Time.fixedDeltaTime);
-        RB.MovePosition(RB.position + movement * Speed * Time.fixedDeltaTime);
-        RB.MoveRotation(targetRotation);
-        
-       // bool isJumping = Input.GetKey(KeyCode.Space);
-       // animator.SetBool("Jump", isJumping);
+        animator.SetBool("Run", globalDirection == Vector3.zero ? false : true);
 
-      //  bool isRunning = Input.GetKey(KeyCode.W);
-       // animator.SetBool("Run", isRunning);
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping == false && isGrounded)
+        {
+            isJumping = true;
+            RB.velocity = new Vector3(RB.velocity.x, JumpVelocity, RB.velocity.z);
+        }
+        
+        animator.SetBool("Jump", isJumping);
+        animator.SetBool("isGrounded", isGrounded);
     }
 
     private void LateUpdate()
     {
-        followCamera.transform.position = RB.position + CameraPos;
+        cameraPivot.transform.position = RB.position + CameraPos;
+        if (rotateCameraWithMouse)
+        {
+            Vector3 delta = mousePos - Input.mousePosition;//x - mouse x, y-axis : mouse y
+            cameraPivot.transform.Rotate(new Vector3(0, -1 * delta.x, 0) * Time.deltaTime * mouseSensitivity);
+            mousePos = Input.mousePosition;
+        }
     }
 }
